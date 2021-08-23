6fdf97bbe4e148d6a96b8d79a4e7c2fbfc8c0ebd
Ismat <ismat.mahmood@softwire.com>
2021-08-23 22:51:56 +0100

    Algorithm optimised and answer generated, problem passed!

--- 3-LargestPrimeFactor.js
+++ 3-LargestPrimeFactor.js
@@ -1,40 +1,33 @@
 function LargestPrime() {
-    // let number = 600851475143;
-    const number = 3453;
+    let number = 600851475143;
+    let largestPrime = 0;
+    let allFactors = getAllFactors(number);
 
-    let sqRoot = Math.ceil(Math.sqrt(number) + 1);
-
-    // console.log(sqRoot);
-    let i = 1;
-
-    let largestPrime = 1;
-
-    while ( i <= sqRoot ) {
-        if (number % i == 0 ) {
-            if ( CheckPrime(i) && i > largestPrime) {
-                largestPrime = i
-            }
-            let oppositeFactor = number / i;
-            if ( CheckPrime( oppositeFactor ) && oppositeFactor > largestPrime) {
-                largestPrime = oppositeFactor
-            }
+    for (i = 0; i < allFactors.length;i++){
+        if (checkPrime(allFactors[i]) && allFactors[i] > largestPrime) {
+            largestPrime = allFactors[i];
         }
-        i++;
     }
-
     console.log(largestPrime);
 }
 
 LargestPrime();
 
-function CheckPrime(num) {
-    let B = num - 1;
+function checkPrime(num) {
+    if (getAllFactors(num).length == 2)
+        return true;
+}
 
-    while (B != 1) {
-        if (num % B == 0) {
-            return false;
+function getAllFactors(number) {
+    const sqRoot = Math.ceil(Math.sqrt(number));
+    let i = 1;
+    let allFactors = [];
+    while (i <= sqRoot) {
+        if (number % i == 0) {
+            allFactors.push(i);
+            allFactors.push(number / i)
         }
-        B--;
+        i++;
     }
-    return true;
+    return allFactors
 }
